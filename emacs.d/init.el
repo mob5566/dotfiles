d1e415a06e536b11ae8780d1d37aa8ba6c7ae26b
code14 <mob5566@gmail.com>
2017-08-01 10:10:49 +0800

    Remove useless files

--- emacs.d/init.el
+++
@@ -1,9 +0,0 @@
-(add-to-list 'load-path "~/.emacs.d/lisp")
-
-(load "personal/packages")
-(load "personal/theme")
-(load "personal/settings")
-
-(let ((custom "~/.emacs_local"))
-  (when (file-exists-p custom)
-    (load-file custom)))
