f5e17ec3106086ed46807274a8a7a118ab6ce121
code14 <mob5566@gmail.com>
2018-05-13 17:22:38 +0800

    [template] import defines from SHIK and betaveros

--- template/pc/temp.cpp
+++ template/pc/temp.cpp
@@ -1,21 +1,125 @@
-/**
- * Author:   Cheng-Shih Wong
- * Email:    mob5566@gmail.com
- * Date:     
- */
+// Code14 - vim:set fdm=marker:
+// {{{
 #include <bits/stdc++.h>
+#include <unistd.h>
 
 using namespace std;
 
+// part of these from betaveros
+#define fori(i,a,b) for(int i = a; i <= int(b); i++)
+#define _conc(x,y) x ## y
+#define _conc2(x,y) _conc(x,y)
+#define rep(n) fori(_conc2(_,__LINE__),1,n)
+#define allof(s) (s).begin(), (s).end()
+#define mkp make_pair
+#define pushb push_back
+#define popb push_back
+#define fi first
+#define sd second
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int,int> PII;
+
+#ifdef DEBUG
+#define debug(code) code
+#define debugf(...) \
+	fprintf(stderr,  __VA_ARGS__)
+
+// from SHIK {{{
+template<typename T>
+void _dump( const char* s, T&& head ) { cerr<<s<<"="<<head<<endl; }
+
+template<typename T, typename... Args>
+void _dump( const char* s, T&& head, Args&&... tail ) {
+    int c=0;
+    while ( *s!=',' || c!=0 ) {
+        if ( *s=='(' || *s=='[' || *s=='{' ) c++;
+        if ( *s==')' || *s==']' || *s=='}' ) c--;
+        cerr<<*s++;
+    }
+    cerr<<"="<<head<<", ";
+    _dump(s+1,tail...);
+}
+
+#define dump(...) do { \
+    fprintf(stderr, "%s:%d - ", __PRETTY_FUNCTION__, __LINE__); \
+    _dump(#__VA_ARGS__, __VA_ARGS__); \
+} while (0)
+
+template<typename Iter>
+ostream& _out( ostream &s, Iter b, Iter e ) {
+    s<<"[";
+    for ( auto it=b; it!=e; it++ ) s<<(it==b?"":" ")<<*it;
+    s<<"]";
+    return s;
+}
+
+template<typename A, typename B>
+ostream& operator <<( ostream &s, const pair<A,B> &p ) { return s<<"("<<p.first<<","<<p.second<<")"; }
+template<typename T>
+ostream& operator <<( ostream &s, const vector<T> &c ) { return _out(s,allof(c)); }
+template<typename T, size_t N>
+ostream& operator <<( ostream &s, const array<T,N> &c ) { return _out(s,allof(c)); }
+template<typename T>
+ostream& operator <<( ostream &s, const set<T> &c ) { return _out(s,allof(c)); }
+template<typename A, typename B>
+ostream& operator <<( ostream &s, const map<A,B> &c ) { return _out(s,allof(c)); }
+// from SHIK }}}
+#else
+#define debug(code)
+#define debugf(...)
+#define dump(...)
+#endif
+
+// from SHIK {{{
+template<typename T>
+void _R( T &x ) { cin>>x; }
+void _R( int &x ) { scanf("%d",&x); }
+void _R( long long &x ) { scanf("%lld",&x); }
+void _R( double &x ) { scanf("%lf",&x); }
+void _R( char &x ) { scanf(" %c",&x); }
+void _R( char *x ) { scanf("%s",x); }
+
+void R() {}
+template<typename T, typename... U>
+void R( T& head, U&... tail ) {
+    _R(head);
+    R(tail...);
+}
+
+template<typename T>
+void _W( const T &x ) { cout<<x; }
+void _W( const int &x ) { printf("%d",x); }
+void _W( const long long &x ) { printf("%lld",x); }
+template<typename T>
+void _W( const vector<T> &x ) {
+    for ( auto i=x.cbegin(); i!=x.cend(); i++ ) {
+        if ( i!=x.cbegin() ) putchar(' ');
+        _W(*i);
+    }
+}
+
+void W() {}
+template<typename T, typename... U>
+void W( const T& head, const U&... tail ) {
+    _W(head);
+    putchar(sizeof...(tail)?' ':'\n');
+    W(tail...);
+}
+// from SHIK }}}
+// }}}
+
 int main(int argc, char *argv[]) {
-	int tt, ti;
+	int tt;
 
-	scanf("%d", &tt);
+	R(tt);
 
-	for (ti = 1; ti <= tt; ++ti) {
+	fori (ti, 1, tt) {
 		printf("Case #%d:", ti);
 
-		fprintf(stderr, "## test case %d solved at %.3f sec ##\n",
+		debugf("## test case %d solved at %.3f sec ##\n",
 			ti, (double)clock()/CLOCKS_PER_SEC);
 	}
 
